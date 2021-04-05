8be2ac5ef3fcdb0693a90fc908069c1776fa5964
Ibrahim-Khaleel <khaleel2977@gmail.com>
2021-04-05 17:56:59 +0530

    added

--- PhotoGallery/myapp/models.py
+++ PhotoGallery/myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(max_length=100)
+    
+
+class Item(models.Model):
+    item = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="media/")
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=100)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.item
+

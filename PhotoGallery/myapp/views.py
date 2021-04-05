4b9b2c6fb619400f6898c222a13d89c17c667ea9
Ibrahim-Khaleel <khaleel2977@gmail.com>
2021-04-05 17:12:10 +0530

    template added

--- PhotoGallery/myapp/views.py
+++ PhotoGallery/myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
+
+def test(request):
+    return HttpResponse("hi")
+
+def index(request):
+    return render(request,"index.html")
+
